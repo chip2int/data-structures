1ae39b6e3b215972d746a6d376a5511e51658ed9
Hack Reactor Students <jdtoney@gmail.com>
2013-11-13 15:37:56 -0800

    copied code from functional to functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,40 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var counter = 0;
+
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+    //push
+    storage[counter]= value;
+    counter++;
+
+  };
+
+  instance.dequeue = function(){
+    //shift
+    if (counter >0){
+      counter--;
+      var holder = storage[0];
+      delete storage[0];
+      //move everything else up
+      for ( i = 0; i < counter; i++){
+        storage[i] = storage[i+1];
+      }
+      return holder;
+    }
+  };
+
+  instance.size = function(){
+    //length
+    return counter;
+  };
+
+  return instance;
 };
 
 var queueMethods = {};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,32 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+var instance = {};
+
+  // Use an object with numeric keys to store values
+  storage = {};
+  counter = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+
+  instance.push = function(value){
+    storage[instance.size()] = value;
+    counter++;
+  };
+
+  instance.pop = function(){
+    if(counter > 0){
+      counter--;
+      var holder = storage[instance.size()];
+      delete storage[instance.size()];
+      return holder;
+    }
+  };
+
+  instance.size = function(){
+    return counter;
+  };
+
+  return instance;
 };
 
 var stackMethods = {};
