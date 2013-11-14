1ac8705384922e5ce28bfa98dc6c41ef5524989e
Hack Reactor Students <jdtoney@gmail.com>
2013-11-13 16:31:58 -0800

    final implementation of stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,31 @@
+var stackMethods = {
+  push:function(value){
+    this.storage[this.size()] = value;
+    this.counter++;
+  },
+
+  pop:function(){
+    if(this.counter > 0){
+      this.counter--;
+      var holder = this.storage[this.size()];
+      delete this.storage[this.size()];
+      return holder;
+    }
+  },
+
+  size:function(){
+    return this.counter;
+  }
+
+};
+
+
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.counter = 0;
 };
+
+Stack.prototype.push = stackMethods.push;
+Stack.prototype.pop = stackMethods.pop;
+Stack.prototype.size = stackMethods.size;
