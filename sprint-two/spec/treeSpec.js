952a80cc0eb345ae017ec149be214858cf0263ad
Hack Reactor Students <jdtoney@gmail.com>
2013-11-14 11:25:53 -0800

    added tests for values in children and children of children and then modified contains so that it passes- Final version of tree.js and treeSpec.js

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -11,5 +11,28 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
+  it("when the tree is empty, contains returns false", function() {
+    expect(tree.contains(5)).toBe(false);
+  });
+
+ it("Setting value to the first node", function() {
+    treeB = makeTree(1);
+    expect(treeB.contains(1)).toBe(true);
+  });
+
+  it("Adding add child with a value and then checking contains for that value returns true", function() {
+    tree.addChild(5);
+    expect(tree.contains(5)).toBe(true);
+  });
+
+
+  it("Adding children and grand children", function() {
+    tree.addChild(0);
+    tree.addChild("Hello");
+    tree.addChild(false);
+    tree.children[1].addChild(5);
+    expect(tree.contains(5)).toBe(true);
+  });
+
   // Add more tests here to test the functionality of tree.
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,29 @@
-var makeTree = function(){
-  var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
+
+var makeTree = function(value){
+  var newTree = Object.create(treeMethods);
+  newTree.value = value;
+  newTree.children = [];
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  var temp = makeTree(); // generate a new tree
+  temp.value = value;
+  this.children.push(temp) ;
+  //this lets you specify target only by referencing arrays e.g. this.children[1].children[0].addChild(). Do we want to target in some other way- value perhas?
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(target){
+  var result = false;
+  if(this.value === target){
+    return true;
+  } else {
+    for( var i = 0; i < this.children.length ; i++){
+      result = result || this.children[i].contains(target);
+    }
+  }
+  return result;
 };
