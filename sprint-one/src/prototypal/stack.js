7ce4e84caa7662216c5106fb06db1e83b4cf55c6
Hack Reactor Students <jdtoney@gmail.com>
2013-11-13 16:09:08 -0800

    replace instance.<method> with Object.create(stackmethods) - final revision

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,34 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+// Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.counter = 0; // Hint: set an initial value here
+
+  return instance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push:function(value){
+    this.storage[this.size()] = value;
+    this.counter++;
+  },
+
+  pop:function(){
+    if(this.counter > 0){
+      this.counter--;
+      var holder = this.storage[this.size()];
+      delete this.storage[this.size()];
+      return holder;
+    }
+  },
+
+  size:function(){
+    return this.counter;
+  }
+
+};
+
+
